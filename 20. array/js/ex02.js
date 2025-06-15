463e98b66e15754730b905502ab61bf097c46467
Hoang An Unicode <hoangan.web@gmail.com>
2025-06-15 10:54:34 +0700

    Array method 01

---
+++ 20. array/js/ex02.js
@@ -0,0 +1,39 @@
+console.log(Array.prototype);
+const users = ["User 1", "User 2", "User 3", "User 4"];
+console.log(users);
+
+// const newArr = users.slice(0, 2);
+// const newArr = users.slice(1);
+// const newArr = users.slice(-2, -1);
+// console.log(newArr);
+
+// const newArr = users.splice(1, 1, "Item 1", "Item 2");
+// console.log(users);
+// console.log(newArr);
+
+// const products = [
+//   "Product 1",
+//   "Product 2",
+//   "Product 3",
+//   "Product 4",
+//   "Product 5",
+// ];
+// const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+// const newArr = users.concat(products, items);
+// console.log(newArr);
+
+// const count = users.push("Item 1", "Item 2", "Item 3");
+// console.log(count);
+// console.log(users);
+
+// const value = users.pop();
+// console.log(value);
+// console.log(users);
+
+// const value = users.shift();
+// console.log(value);
+// console.log(users);
+
+// const count = users.unshift("Item 1", "Item 2", "Item 3");
+// console.log(count);
+// console.log(users);
